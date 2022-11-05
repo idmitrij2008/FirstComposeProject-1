f25ace769a631977164ec3c5e137fbd442b636d1
Журба Дмитрий <dmitriy.zhurba@gazprombank.ru>
2022-11-05 17:09:55 +0600

    lesson_13

--- app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.sumin.firstcomposeproject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import com.sumin.firstcomposeproject.ui.theme.FirstComposeProjectTheme
@@ -18,13 +23,22 @@ class MainActivity : ComponentActivity() {
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         setContent {
-            FirstComposeProjectTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background)
-                ) {
-                    InstagramProfileCard(viewModel)
+            Test(viewModel = viewModel)
+        }
+    }
+}
+
+@Composable
+private fun Test(viewModel: MainViewModel) {
+    FirstComposeProjectTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)
+        ) {
+            LazyColumn {
+                items(500) {
+                    InstagramProfileCard(viewModel = viewModel)
                 }
             }
         }
